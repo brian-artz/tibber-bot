c91502ec2f1dc3530456bc8114e94480c8c1d56d
Brian Artz <brian.dev.artz@gmail.com>
2023-10-17 23:09:11 +0200

    Adds test proj to sln + rename Start -> Position

--- TibberBot/Dto/EnterPathRequest.cs
+++ TibberBot/Dto/EnterPathRequest.cs
@@ -5,19 +5,19 @@ namespace TibberBot.Dto
     public record EnterPathRequest
     {
         [JsonPropertyName("start")]
-        public Start Start { get; set; }
+        public Position Start { get; set; }
         
         [JsonPropertyName("commands")]
         public IEnumerable<Command> Commands { get; set; } = Array.Empty<Command>(); 
 
-        public EnterPathRequest(Start start, IEnumerable<Command> commands)
+        public EnterPathRequest(Position start, IEnumerable<Command> commands)
         {
             Start = start;
             Commands = commands;
         }
     };
 
-    public record Start(int X, int Y);
+    public record Position(int X, int Y);
 
     public record Command(string Direction, int Steps);
 
