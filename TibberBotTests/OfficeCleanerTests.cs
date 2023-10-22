8ab8fe3526662c6c607e4a49d193f690ed8afb6e
Brian Artz <brian.dev.artz@gmail.com>
2023-10-22 18:45:49 +0200

    Adds tests for Position + OfficeCleaner

---
+++ TibberBotTests/OfficeCleanerTests.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using TibberBot.Cleaners;
+using TibberBot.Dto;
+
+namespace TibberBotTests
+{
+    public class OfficeCleanerTests
+    {
+        private ICleaner _officeCleaner;
+        private readonly Position _home = new(0, 0);
+
+        [SetUp]
+        public void Setup()
+        {
+            var mockLogger = new Mock<ILogger<OfficeCleaner>>();
+            _officeCleaner = new OfficeCleaner(mockLogger.Object);
+        }
+
+        [Test]
+        public void Test_Clean_Four_Corners()
+        {
+            var commandsList = new List<Command>()
+            {
+                { new Command("north", 100_000) },
+                { new Command("east", 100_000) },
+                { new Command("south", 100_000) },
+                { new Command("south", 100_000) },
+                { new Command("west", 100_000) },
+                { new Command("west", 100_000) },
+                { new Command("north", 100_000) },
+                { new Command("north", 100_000) },
+            };
+
+            var expected = new ExecutionRecord()
+            {
+                StartingPosition = _home,
+                Commands = commandsList.Count,
+                Result = 800001,
+                EndingPosition = new Position(-100_000, 100_000)
+            };
+
+            var record = _officeCleaner.Clean(_home, commandsList);
+            Assert.That(record, Is.Not.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(record.Commands, Is.EqualTo(expected.Commands));
+                Assert.That(record.Result, Is.EqualTo(expected.Result));
+                Assert.That(record.StartingPosition, Is.EqualTo(expected.StartingPosition));
+                Assert.That(record.EndingPosition, Is.EqualTo(expected.EndingPosition));
+            });
+        }
+
+        [Test]
+        public void Test_Clean_Small_Area()
+        {
+            var commandsList = new List<Command>()
+            {
+                { new Command("north", 100) },
+                { new Command("east", 1000) },
+                { new Command("west", 1500) },
+                { new Command("south", 30) },
+                { new Command("south", 2000) },
+            };
+
+            var expected = new ExecutionRecord()
+            {
+                StartingPosition = _home,
+                Commands = commandsList.Count,
+                Result = 3631,
+                EndingPosition = new Position(-500, -1930)
+            };
+
+            var record = _officeCleaner.Clean(_home, commandsList);
+            Assert.That(record, Is.Not.Null);
+            Assert.Multiple(() =>
+            {
+                Assert.That(record.Commands, Is.EqualTo(expected.Commands));
+                Assert.That(record.Result, Is.EqualTo(expected.Result));
+                Assert.That(record.StartingPosition, Is.EqualTo(expected.StartingPosition));
+                Assert.That(record.EndingPosition, Is.EqualTo(expected.EndingPosition));
+            });
+        }
+
+        [TestCase("north")]
+        [TestCase("south")]
+        [TestCase("east")]
+        [TestCase("west")]
+        public void Test_Clean_Out_Of_Bounds(string direction)
+        {
+            var commandsList = new List<Command>()
+            {
+                { new Command(direction, 50_000) },
+                { new Command(direction, 50_000) },
+                { new Command(direction, 1) },
+            };
+            var record = _officeCleaner.Clean(_home, commandsList);
+            Assert.That(record, Is.Null);
+        }
+
+        [Test]
+        public void Test_Clean_No_Commands()
+        {
+            var record = _officeCleaner.Clean(_home, Array.Empty<Command>());
+            Assert.That(record, Is.Null);
+        }
+    }
+}
---
+++ TibberBotTests/PositionTests.cs
@@ -0,0 +1,158 @@
+using TibberBot.Dto;
+using TibberBot.Helpers;
+
+namespace TibberBotTests
+{
+    public class PositionTests
+    {
+        private readonly Position _home = new(0, 0);
+
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        [TestCase(100000)]
+        [TestCase(100000000)]
+        public void Test_Calculate_New_Position_North(int steps)
+        {
+            var newPos = _home.Calculate("north", steps);
+            Assert.Multiple(() =>
+            {
+                Assert.That(newPos, Is.Not.EqualTo(_home));
+                Assert.That(newPos.Y, Is.EqualTo(_home.Y + steps));
+            });
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        [TestCase(100000)]
+        [TestCase(100000000)]
+        public void Test_Calculate_New_Position_South(int steps)
+        {
+            var newPos = _home.Calculate("south", steps);
+            Assert.Multiple(() =>
+            {
+                Assert.That(newPos, Is.Not.EqualTo(_home));
+                Assert.That(newPos.Y, Is.EqualTo(_home.Y - steps));
+            });
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        [TestCase(100000)]
+        [TestCase(100000000)]
+        public void Test_Calculate_New_Position_East(int steps)
+        {
+            var newPos = _home.Calculate("east", steps);
+            Assert.Multiple(() =>
+            {
+                Assert.That(newPos, Is.Not.EqualTo(_home));
+                Assert.That(newPos.X, Is.EqualTo(_home.X + steps));
+            });
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        [TestCase(100000)]
+        [TestCase(100000000)]
+        public void Test_Calculate_New_Position_West(int steps)
+        {
+            var newPos = _home.Calculate("west", steps);
+            Assert.Multiple(() =>
+            {
+                Assert.That(newPos, Is.Not.EqualTo(_home));
+                Assert.That(newPos.X, Is.EqualTo(_home.X - steps));
+            });
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        [TestCase(100000)]
+        [TestCase(100000000)]
+        public void Test_Enumerate_New_Positions_North(int steps)
+        {
+            var newPosArr = _home.EnumerateNewPositions("north", steps).ToArray();
+            Assert.Multiple(() =>
+            {
+                Assert.That(newPosArr, Is.Not.Empty);
+                Assert.That(newPosArr, Has.Length.EqualTo(steps));
+                Assert.That(newPosArr.All(p => _home.Y < p.Y));
+                Assert.That(newPosArr.Max(p => p.Y), Is.EqualTo(_home.Y + steps));
+                Assert.That(newPosArr.Min(p => p.Y), Is.EqualTo(_home.Y + 1));
+            });
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        [TestCase(100000)]
+        [TestCase(100000000)]
+        public void Test_Enumerate_New_Positions_South(int steps)
+        {
+            var newPosArr = _home.EnumerateNewPositions("south", steps).ToArray();
+            Assert.Multiple(() =>
+            {
+                Assert.That(newPosArr, Is.Not.Empty);
+                Assert.That(newPosArr, Has.Length.EqualTo(steps));
+                Assert.That(newPosArr.All(p => _home.Y > p.Y));
+                Assert.That(newPosArr.Max(p => p.Y), Is.EqualTo(_home.Y - 1));
+                Assert.That(newPosArr.Min(p => p.Y), Is.EqualTo(_home.Y - steps));
+            });
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        [TestCase(100000)]
+        [TestCase(100000000)]
+        public void Test_Enumerate_New_Positions_East(int steps)
+        {
+            var newPosArr = _home.EnumerateNewPositions("east", steps).ToArray();
+            Assert.Multiple(() =>
+            {
+                Assert.That(newPosArr, Is.Not.Empty);
+                Assert.That(newPosArr, Has.Length.EqualTo(steps));
+                Assert.That(newPosArr.All(p => _home.X < p.X));
+                Assert.That(newPosArr.Max(p => p.X), Is.EqualTo(_home.X + steps));
+                Assert.That(newPosArr.Min(p => p.X), Is.EqualTo(_home.X + 1));
+            });
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        [TestCase(100000)]
+        [TestCase(100000000)]
+        public void Test_Enumerate_New_Positions_West(int steps)
+        {
+            var newPosArr = _home.EnumerateNewPositions("west", steps).ToArray();
+            Assert.Multiple(() =>
+            {
+                Assert.That(newPosArr, Is.Not.Empty);
+                Assert.That(newPosArr, Has.Length.EqualTo(steps));
+                Assert.That(newPosArr.All(p => _home.X > p.X));
+                Assert.That(newPosArr.Max(p => p.X), Is.EqualTo(_home.X - 1));
+                Assert.That(newPosArr.Min(p => p.X), Is.EqualTo(_home.X - steps));
+            });
+        }
+    }
+}
